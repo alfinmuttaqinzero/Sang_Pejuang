0637281cfce834d2717c643fd9ec8f2294f8b41b
Ntune0241 <129527507+Ntune0241@users.noreply.github.com>
2023-11-11 23:16:13 +0700

    Pause

---
+++ Assets/pauseMMMM.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Add this using directive
+
+public class pausemenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("pauseicon");
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+}
